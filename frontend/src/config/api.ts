a1f694a5d3845f63cc6d271f8f24c9b9cae1dbb8
Chenfei <chenfei.xiong@outlook.com>
2024-12-10 21:10:09 +0100

    config frontend for deployment

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.PROD 
+export const API_BASE_URL = import.meta.env.VITE_MODE === 'production'
   ? import.meta.env.VITE_API_URL_PROD
   : import.meta.env.VITE_API_URL_DEV;
 
